e219321f2ec7bd4e1aab7c9742f61af97b601508
Fotios Lindiakos <fotios@redhat.com>
2012-06-29 17:03:58 -0400

    Adding initial Rails code:   rails new .
    Created Gemfile.lock:
      bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '8fe7bae81006a9af2396398f52967e0d42e138eb74692c43df9453faecc0c267949c1099917845a81323ded3446da79b2b5d41187abf81d99c93800e25f1c9bc'
